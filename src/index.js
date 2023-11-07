8c5c23918aaffd87f9d81f1e593293b6902a54d1
Nausicaa <nausicaa.lusano@nedap.com>
2023-11-07 14:39:29 +0100

    Use overmind

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createOvermind } from "overmind";
+import { Provider } from "overmind-react";
+import { config } from "./overmind";
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const overmind = createOvermind(config);
+
 root.render(
-  <React.StrictMode>
+  <Provider value={overmind}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.querySelector("#app")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
