9fbb05292fc8029183765a2059e05bbdfe579aba
Nausicaa <nausicaa.lusano@nedap.com>
2023-11-09 17:28:02 +0100

    Update Todo component, add strikethrough styling

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,9 +2,10 @@ import * as React from "react";
 import { useAppState } from "../overmind";
 
 const Todo = React.memo(({ id }) => {
-  const todo = useAppState((state) => state.todos[id]);
+  const todo = useAppState((state) => state.todosList[id]);
+  const todoTitle = !todo.isDone ? todo.title : <s>{todo.title}</s>;
 
-  return <li>{todo.title}</li>;
+  return <li>{todoTitle}</li>;
 });
 
 export default Todo;
