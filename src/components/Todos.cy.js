eea43aae5d57119586de50e15c89d2fde7238431
Nausicaa <nausicaa.lusano@nedap.com>
2023-11-09 19:11:31 +0100

    Split concerns of toggle test Remove overmind initialization from beforeEach hook; experiment with different initial states

--- src/components/Todos.cy.js
+++ src/components/Todos.cy.js
@@ -9,7 +9,32 @@ describe("<Todos />", () => {
     { id: 2, title: "Do the dishes", isDone: true },
     { id: 3, title: "Go for a walk", isDone: false },
   ];
-  beforeEach(() => {
+  // beforeEach(() => {
+  //   const overmind = createOvermindMock(config, (state) => {
+  //     state.todos = todoList;
+  //     state.todosList = derived((state) => {
+  //       const list = state.showAllTodos
+  //         ? state.todos
+  //         : state.todos.filter((todo) => !todo.isDone);
+  //       return list.reduce(
+  //         (obj, i) => ({
+  //           ...obj,
+  //           [i.id]: i,
+  //         }),
+  //         {}
+  //       );
+  //     });
+  //     state.showAllTodos = true;
+  //   });
+
+  //   cy.mount(
+  //     <Provider value={overmind}>
+  //       <Todos />
+  //     </Provider>
+  //   );
+  // });
+
+  it("should display the full list on mount", () => {
     const overmind = createOvermindMock(config, (state) => {
       state.todos = todoList;
       state.todosList = derived((state) => {
@@ -32,13 +57,32 @@ describe("<Todos />", () => {
         <Todos />
       </Provider>
     );
-  });
-
-  it("should display the full list on mount", () => {
     cy.get("li").should("have.length", 3);
   });
 
-  it("should toggle the list filter when clicking on the checkbox", () => {
+  it("should filter the list when checking the checkbox", () => {
+    const overmind = createOvermindMock(config, (state) => {
+      state.todos = todoList;
+      state.todosList = derived((state) => {
+        const list = state.showAllTodos
+          ? state.todos
+          : state.todos.filter((todo) => !todo.isDone);
+        return list.reduce(
+          (obj, i) => ({
+            ...obj,
+            [i.id]: i,
+          }),
+          {}
+        );
+      });
+      state.showAllTodos = true;
+    });
+
+    cy.mount(
+      <Provider value={overmind}>
+        <Todos />
+      </Provider>
+    );
     cy.get("input[type=checkbox]")
       .should("not.be.checked")
       .check()
@@ -47,6 +91,31 @@ describe("<Todos />", () => {
       expect($li).to.have.length(1);
       expect($li).to.have.text("Go for a walk");
     });
+  });
+
+  it("should unfilter the list filter when unchecking the checkbox", () => {
+    const overmind = createOvermindMock(config, (state) => {
+      state.todos = todoList;
+      state.todosList = derived((state) => {
+        const list = state.showAllTodos
+          ? state.todos
+          : state.todos.filter((todo) => !todo.isDone);
+        return list.reduce(
+          (obj, i) => ({
+            ...obj,
+            [i.id]: i,
+          }),
+          {}
+        );
+      });
+      state.showAllTodos = false;
+    });
+
+    cy.mount(
+      <Provider value={overmind}>
+        <Todos showAll={false} />
+      </Provider>
+    );
     cy.get("input[type=checkbox]")
       .should("be.checked")
       .uncheck()
@@ -59,4 +128,26 @@ describe("<Todos />", () => {
         expect($li).to.contain.text(todoList[index].title);
       });
   });
+
+  // it("should toggle the list filter when clicking on the checkbox", () => {
+  //   cy.get("input[type=checkbox]")
+  //     .should("not.be.checked")
+  //     .check()
+  //     .should("be.checked");
+  //   cy.get("li").should(($li) => {
+  //     expect($li).to.have.length(1);
+  //     expect($li).to.have.text("Go for a walk");
+  //   });
+  //   cy.get("input[type=checkbox]")
+  //     .should("be.checked")
+  //     .uncheck()
+  //     .should("not.be.checked");
+  //   cy.get("li")
+  //     .then(($li) => {
+  //       expect($li).to.have.length(todoList.length);
+  //     })
+  //     .each(($li, index) => {
+  //       expect($li).to.contain.text(todoList[index].title);
+  //     });
+  // });
 });
