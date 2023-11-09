115c9fda54781043cb5a415d248e809c76d9e369
Nausicaa <nausicaa.lusano@nedap.com>
2023-11-09 11:00:34 +0100

    Add comp test for counter elem with overmind

---
+++ src/components/CounterOvermind.cy.js
@@ -0,0 +1,20 @@
+import { createOvermindMock } from "overmind";
+import { Provider } from "overmind-react";
+import { config } from "../overmind";
+import CounterOvermind from "./CounterOvermind";
+
+describe("<CounterOvermind />", () => {
+  it("when the increment button is pressed, the counter is incremented", () => {
+    const overmind = createOvermindMock(config, (state) => {
+      state.count = 15;
+    });
+
+    cy.mount(
+      <Provider value={overmind}>
+        <CounterOvermind />
+      </Provider>
+    );
+    cy.get("[data-cy=overmind-increment]").click();
+    cy.get("[data-cy=overmind-counter]").should("have.text", "16");
+  });
+});
