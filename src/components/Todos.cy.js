fd19a35e11cda5cd1c3a9b6d1d249e98467f8e1b
Nausicaa <nausicaa.lusano@nedap.com>
2023-11-09 19:12:01 +0100

    Cleanup

--- src/components/Todos.cy.js
+++ src/components/Todos.cy.js
@@ -9,30 +9,6 @@ describe("<Todos />", () => {
     { id: 2, title: "Do the dishes", isDone: true },
     { id: 3, title: "Go for a walk", isDone: false },
   ];
-  // beforeEach(() => {
-  //   const overmind = createOvermindMock(config, (state) => {
-  //     state.todos = todoList;
-  //     state.todosList = derived((state) => {
-  //       const list = state.showAllTodos
-  //         ? state.todos
-  //         : state.todos.filter((todo) => !todo.isDone);
-  //       return list.reduce(
-  //         (obj, i) => ({
-  //           ...obj,
-  //           [i.id]: i,
-  //         }),
-  //         {}
-  //       );
-  //     });
-  //     state.showAllTodos = true;
-  //   });
-
-  //   cy.mount(
-  //     <Provider value={overmind}>
-  //       <Todos />
-  //     </Provider>
-  //   );
-  // });
 
   it("should display the full list on mount", () => {
     const overmind = createOvermindMock(config, (state) => {
@@ -128,26 +104,4 @@ describe("<Todos />", () => {
         expect($li).to.contain.text(todoList[index].title);
       });
   });
-
-  // it("should toggle the list filter when clicking on the checkbox", () => {
-  //   cy.get("input[type=checkbox]")
-  //     .should("not.be.checked")
-  //     .check()
-  //     .should("be.checked");
-  //   cy.get("li").should(($li) => {
-  //     expect($li).to.have.length(1);
-  //     expect($li).to.have.text("Go for a walk");
-  //   });
-  //   cy.get("input[type=checkbox]")
-  //     .should("be.checked")
-  //     .uncheck()
-  //     .should("not.be.checked");
-  //   cy.get("li")
-  //     .then(($li) => {
-  //       expect($li).to.have.length(todoList.length);
-  //     })
-  //     .each(($li, index) => {
-  //       expect($li).to.contain.text(todoList[index].title);
-  //     });
-  // });
 });
