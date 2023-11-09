61a36666230d4992493d2f792a001e4b1e08a71b
Nausicaa <nausicaa.lusano@nedap.com>
2023-11-09 13:40:15 +0100

    Update form component test spec

--- src/components/FormOvermind.cy.js
+++ src/components/FormOvermind.cy.js
@@ -4,6 +4,20 @@ import { config } from "../overmind";
 import FormOvermind from "./FormOvermind";
 
 describe("<FormOvermind />", () => {
+  // beforeEach(() => {
+  //   const overmind = createOvermindMock(config, (state) => {
+  //     state.form.fname = "";
+  //     state.form.lname = "";
+  //     state.form.isValid = true;
+  //   });
+
+  //   cy.mount(
+  //     <Provider value={overmind}>
+  //       <FormOvermind />
+  //     </Provider>
+  //   );
+  // })
+
   it("should display validation err message if click on submit with empty form", () => {
     const overmind = createOvermindMock(config, (state) => {
       state.form.fname = "";
@@ -19,8 +33,9 @@ describe("<FormOvermind />", () => {
     cy.get('input[type="submit"]').click();
     cy.get("div#validation")
       .should("be.visible")
-      .and("have.text", "Could not submit");
+      .and("have.text", "Form is empty");
   });
+
   it("should display validation err message if click on submit with empty first name", () => {
     const overmind = createOvermindMock(config, (state) => {
       state.form.fname = "";
@@ -35,8 +50,28 @@ describe("<FormOvermind />", () => {
     );
     cy.get("input#lname").type("Surname");
     cy.get('input[type="submit"]').click();
+
     cy.get("div#validation")
       .should("be.visible")
-      .and("have.text", "Could not submit");
+      .and("have.text", "Enter first name");
+  });
+
+  it("should display validation err message if click on submit with empty last name", () => {
+    const overmind = createOvermindMock(config, (state) => {
+      state.form.fname = "";
+      state.form.lname = "";
+      state.form.isValid = true;
+    });
+
+    cy.mount(
+      <Provider value={overmind}>
+        <FormOvermind />
+      </Provider>
+    );
+    cy.get("input#fname").type("Name");
+    cy.get('input[type="submit"]').click();
+    cy.get("div#validation")
+      .should("be.visible")
+      .and("have.text", "Enter last name");
   });
 });
