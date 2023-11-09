d3d4b97ff05a9c2cfbf772bf6a33757f1b75cc2b
Nausicaa <nausicaa.lusano@nedap.com>
2023-11-09 13:05:37 +0100

    Add form actions To set values for fields, to handle validation

--- src/overmind/actions.js
+++ src/overmind/actions.js
@@ -1,2 +1,10 @@
 export const decrement = ({ state }) => state.count--;
 export const increment = ({ state }) => state.count++;
+export const setFNameValue = ({ state }, val) => (state.form.fname = val);
+export const setLNameValue = ({ state }, val) => (state.form.lname = val);
+export const validateForm = ({ state }) => {
+  if (!state.form.fname || !state.form.lname) {
+    console.log("Cannot submit");
+    state.form.isValid = false;
+  }
+};
