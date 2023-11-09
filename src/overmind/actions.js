2662848bc3970db1707de25eb940b1027262dc03
Nausicaa <nausicaa.lusano@nedap.com>
2023-11-09 13:39:54 +0100

    Add form validation condition cases

--- src/overmind/actions.js
+++ src/overmind/actions.js
@@ -3,8 +3,19 @@ export const increment = ({ state }) => state.count++;
 export const setFNameValue = ({ state }, val) => (state.form.fname = val);
 export const setLNameValue = ({ state }, val) => (state.form.lname = val);
 export const validateForm = ({ state }) => {
-  if (!state.form.fname || !state.form.lname) {
-    console.log("Cannot submit");
+  if (!state.form.fname && !state.form.lname) {
     state.form.isValid = false;
+    state.form.validationErr = "Form is empty";
+  } else {
+    if (!state.form.fname) {
+      state.form.isValid = false;
+      state.form.validationErr = "Enter first name";
+    }
+    if (!state.form.lname) {
+      state.form.isValid = false;
+      state.form.validationErr = "Enter last name";
+    }
   }
+
+  return null;
 };
