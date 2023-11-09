fc8fe3f34950ffc57febb8358fe6196d67e8a81b
Nausicaa <nausicaa.lusano@nedap.com>
2023-11-09 13:30:36 +0100

    Use default mount command

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -15,21 +15,21 @@
 
 // Import commands.js using ES2015 syntax:
 import "./commands";
-import { createOvermind } from "overmind";
-import { config } from "../../src/overmind";
-import { Provider } from "overmind-react";
+// import { createOvermind } from "overmind";
+// import { config } from "../../src/overmind";
+// import { Provider } from "overmind-react";
 
 import { mount } from "cypress/react18";
 
-// Cypress.Commands.add("mount", mount);
+Cypress.Commands.add("mount", mount);
 
 // Example use:
 // cy.mount(<MyComponent />)
 
-Cypress.Commands.add("mount", (component, options = {}) => {
-  const { overmind = createOvermind(config), ...mountOptions } = options;
+// Cypress.Commands.add("mount", (component, options = {}) => {
+//   const { overmind = createOvermind(config), ...mountOptions } = options;
 
-  const wrapped = <Provider value={overmind}>{component}</Provider>;
+//   const wrapped = <Provider value={overmind}>{component}</Provider>;
 
-  return mount(wrapped, mountOptions);
-});
+//   return mount(wrapped, mountOptions);
+// });
