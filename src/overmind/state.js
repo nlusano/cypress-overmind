b7127c00f58c5b21e4b0be957c0a78d60a17fedb
Nausicaa <nausicaa.lusano@nedap.com>
2023-11-09 17:21:52 +0100

    Update state to work on todo list

--- src/overmind/state.js
+++ src/overmind/state.js
@@ -1,11 +1,29 @@
+import { derived } from "overmind";
+
 export const state = {
   // title: "My App",
   count: 0,
   todos: [
-    { id: 1, title: "Create tiny project" },
-    { id: 2, title: "Add cypress to the project" },
-    { id: 3, title: "Play around with component testing" },
+    { id: 1, title: "Create tiny project", isDone: true },
+    { id: 2, title: "Add cypress to the project", isDone: true },
+    { id: 3, title: "Play around with component testing", isDone: true },
+    { id: 4, title: "Water plants", isDone: false },
+    { id: 5, title: "Wash dishes", isDone: false },
+    { id: 6, title: "Study docs", isDone: true },
   ],
+  todosList: derived((state) => {
+    const list = state.showAllTodos
+      ? state.todos
+      : state.todos.filter((todo) => !todo.isDone);
+    return list.reduce(
+      (obj, i) => ({
+        ...obj,
+        [i.id]: i,
+      }),
+      {}
+    );
+  }),
+  showAllTodos: true,
   form: {
     fname: "",
     lname: "",
